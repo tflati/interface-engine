f21b0c920457330e2b47c3c85d1e7b5c6f9766c6
tflati <tflati@di.uniroma1.it>
2017-05-15 18:24:49 +0200

    Added the possibility to append a pedix (image) as well as an image in a table cell.

--- components/card/cardController.js
+++ components/card/cardController.js
@@ -1,11 +1,12 @@
 app.controller("cardController", function($scope){
 	
 	$scope.init = function(data){
+		console.log("CARD DATA", data);
 		$scope.title = data.title;
 		$scope.subtitle = data.subtitle;
 		$scope.type = data.type;
 		$scope.width = data.width;
-		if($scope.width.endsWith("%")) $scope.width = $scope.width.substring(0, $scope.width.length - 1);
+		if($scope.width != undefined && $scope.width.endsWith("%")) $scope.width = $scope.width.substring(0, $scope.width.length - 1);
 		$scope.footer = data.footer;
 		$scope.elements = data.elements;
 		
